fbc651accc7d5e8dda8bf834e6ba9ed5a625295e
Drake24 <zjhassler@gmail.com>
2020-05-04 22:20:23 -0500

    Working app!

--- liri.js
+++ liri.js
@@ -2,62 +2,96 @@ var fs = require("fs");
 require("dotenv").config();
 var axios = require("axios");
 var keys = require("./keys.js");
+//var Spotify = require('node-spotify-api');
 //var spotify = new Spotify(keys.spotify);
+var input = process.argv.slice(3).join(" ");
 
 var command = process.argv[2];
+//command in take switch statement:
+function switchFunc(){
+  switch(command) {
 
-switch(command) {
+      case `concert-this`:
+          concertThis()
+      break;
+      case `spotify-this-song`:
+          spotifyThis()
+      break;
+      case `movie-this`:
+          movieThis();
+      break;
+      case `do-what-it-says`:
+        fs.readFile("random.txt","utf8", function(error, data) {
+          if (error) {
+              return console.log(error);
+          }
+          var dataArr = data.split(',');
+          command = dataArr[0];
+          input = dataArr[1];
+          switchFunc()
 
-    case `concert-this`:
-        concertThis()
-    break;
-    case `spotify-this-song`:
-        spotifyThis()
-    break;
-    case `movie-this`:
-        movieThis();
-    break;
-    case `do-what-it-says`:
-        doWhatitsays()
-    break;
-}
+        });
+          //doWhatitsays()
+      break;
+  };
+};
 function concertThis(){
-    console.log("concert this")
+    var axios = require("axios");
+    //console.log("concert this")
+    var artist =`https://rest.bandsintown.com/artists/${input}/events?app_id=codingbootcamp`
+    axios.get(artist).then(
+      function(response) {
+        console.log(`${input}'s Next Show!!!`)
+        console.log(`Venue: ${response.data[0].venue.name}`);
+        console.log(`Venue Location: ${response.data[0].venue.location}`);
+        console.log(`Date: ${response.data[0].datetime}`);
+      });
 }
+
 function spotifyThis(){
-    console.log("spotify This")
+    var keys = require("./keys.js");
+    var Spotify = require('node-spotify-api');
+    var spotify = new Spotify(keys.spotify);
+    //console.log("Spotify This")
+    if (input === '') {
+      input = 'The Sign'
+    }
+    spotify.search({ type: 'track', query: input, limit: 1})
+    .then(function(response) {
+        console.log(`Tracks Artist! ${response.tracks.items[0].artists[0].name}`);
+        console.log(`Tracks Name! ${response.tracks.items[0].name}`);
+        console.log(`Preview Link! ${response.tracks.items[0].preview_url}`);
+        console.log(`Album! ${response.tracks.items[0].album.name}`);
+      })
+    .catch(function(err) {
+      console.log(err);
+    });
 }
 function movieThis(){
     var axios = require("axios");
-    console.log("movie this is running")
-    var nodeArgs = process.argv;
-
-// Create an empty variable for holding the movie name
-var movieName = "";
-
-// Loop through all the words in the node argument
-// And do a little for-loop magic to handle the inclusion of "+"s
-for (var i = 3; i < nodeArgs.length; i++) {
-  if (i > 3 && i < nodeArgs.length) {
-    movieName = movieName + "+" + nodeArgs[i];
-  } else {
-    movieName += nodeArgs[i];
-  }
+    //console.log("movie this is running")
+ // Create an empty variable for holding the movie name
+if (input === '') {
+  input = 'Mr. Nobody'
 }
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-// This line is just to help us debug against the actual URL.
-console.log(queryUrl);
-//
+var queryUrl = `http://www.omdbapi.com/?t= ${input} &y=&plot=short&apikey=trilogy`;
+
 axios.get(queryUrl).then(
   function(response) {
-    console.log(response.data, '\n',
-        "Release Year: " + response.data.Year);
-    //console.log('',a,'\n',b,'\n',c)
+    //console.log(response.data);
+    console.log(
+        "Title: ", response.data.Title, '\n',
+        "Year: ", response.data.Year, '\n',
+        "IMDB Rating: ", response.data.imdbRating, '\n',
+        "Rotten Tomatoes Rating: ", response.data.Ratings[1].Value, '\n',
+        "Country of Production: ", response.data.Country, '\n',
+        "Languages: ", response.data.Language, '\n',
+        "Plot: ", response.data.Plot, '\n',
+        "Cast: ", response.data.Actors 
+        );
   })
   .catch(function(error) {
     if (error.response) {
-      // The request was made and the server responded with a status code
-      // that falls out of the range of 2xx
       console.log("---------------Data---------------");
       console.log(error.response.data);
       console.log("---------------Status---------------");
@@ -65,8 +99,6 @@ axios.get(queryUrl).then(
       console.log("---------------Status---------------");
       console.log(error.response.headers);
     } else if (error.request) {
-      // The request was made but no response was received
-      // `error.request` is an object that comes back with details pertaining to the error that occurred.
       console.log(error.request);
     } else {
       // Something happened in setting up the request that triggered an Error
@@ -74,8 +106,5 @@ axios.get(queryUrl).then(
     }
     console.log(error.config);
   });
-
 }
-function doWhatitsays() {
-    console.log("do what it says")
-}
+switchFunc()
